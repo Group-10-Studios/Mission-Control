1ab51cc6dfd2e019cc6f16d91ff09ea1261ceafd
Ahad Rahman <rahmanahad@myvuw.ac.nz>
2020-03-18 16:07:58 +1300

    #0: Test commit

---
+++ src/test.js
@@ -0,0 +1,17 @@
+class Person {
+	constructor(name, age, school="Victoria") {
+		this.name = name;
+		this.age = age;
+		this.school = school;
+	}
+
+	addAge(toAdd) {
+		this.age += toAdd;
+	}
+}
+
+var p = new Person("jake", 25);
+p.addAge(4)
+
+console.log("SCHOOL: " + p.school);
+console.log("AGE: " + p.age);
