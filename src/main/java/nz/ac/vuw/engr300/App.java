47810cece4480b2719c3c943f33e049e3d4e77c2
Nathan Duckett <nduckett13@gmail.com>
2020-04-04 17:37:57 +1300

    Basic logging functionality in App

--- src/main/java/nz/ac/vuw/engr300/App.java
+++ src/main/java/nz/ac/vuw/engr300/App.java
@@ -1,7 +1,12 @@
 package nz.ac.vuw.engr300;
 
+import org.apache.log4j.Logger;
+
 public class App {
+  private static final Logger logger = Logger.getLogger(App.class);
+
   public boolean getStatus() {
+    logger.info("Getting status: True");
     return true;
   }
   
@@ -10,6 +15,7 @@ public class App {
   }
   
   public static void main(String[] args) {
+    logger.info("Application Starting");
     new App().printHello();
   }
 }
