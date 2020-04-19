a18fbde330ad706e691f7e1b08bc73e8a4c8a38d
Jake Mai <maiquan@ecs.vuw.ac.nz>
2020-04-19 20:51:44 +1200

    #13: Pulling weather data from OpenWeatherAPI and output to a json file

---
+++ src/main/java/nz/ac/vuw/engr300/weather/importers/PullWeatherApi.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+public class PullWeatherApi {
+
+    public static void main(String[] args) throws MalformedURLException {
+       String apiKey = "cc5a7a7ec97d0a02e950717c0e632548";
+       String cityID = "2179538"; //Wellington
+       String returnedData = "";
+
+       try{
+            String apiCall = "https://api.openweathermap.org/data/2.5/forecast?id="+cityID+"&appid="+apiKey;
+
+            // Fetch data
+            URL useThisURL = new URL(apiCall);
+            URLConnection connect = useThisURL.openConnection();
+            BufferedReader bReader = new BufferedReader((new InputStreamReader(connect.getInputStream())));
+            returnedData += bReader.readLine();
+
+            // Write data to json
+            BufferedWriter writer = new BufferedWriter(new FileWriter("./output.json")); //Nathan: feel free to modify the output file location.
+            writer.write(returnedData);
+
+            writer.close();
+            bReader.close();
+
+       } catch (IOException e){
+           System.out.println("Error "+ e);
+       }
+    }
+}
