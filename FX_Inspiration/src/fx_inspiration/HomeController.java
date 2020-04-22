f2e0ad8eb2bb93358bbc7666ae8b5e937f8daa0e
Nalin Aswani <38874717+Nalinboi@users.noreply.github.com>
2020-04-22 12:58:28 +1200

    Added netbeans project

---
+++ FX_Inspiration/src/fx_inspiration/HomeController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fx_inspiration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Nalin
+ */
+public class HomeController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+//        Node [] nodes = new Node[15];
+        
+//        for(int i = 0; i<15; i++){
+//            nodes[i]=(Node)FXMLLoader.load(getclass().getResource("Item.fxmk"));
+//        }          
+    }    
+    
+}
