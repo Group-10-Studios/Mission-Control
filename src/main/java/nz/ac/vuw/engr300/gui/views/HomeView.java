172fd4986e8f78ce0337d453c53b993d48021b72
Timothy Salisbury <salisbtimo@ecs.vuw.ac.nz>
2020-05-03 13:26:04 +1200

    \#21: Added null check for home view

--- src/main/java/nz/ac/vuw/engr300/gui/views/HomeView.java
+++ src/main/java/nz/ac/vuw/engr300/gui/views/HomeView.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 /**
  * Represents a Home Screen view which the application will default to.
@@ -16,8 +17,13 @@ public class HomeView {
 
     public HomeView(Stage stage) {
         Parent root = null;
+        URL layoutFile = getClass().getClassLoader().getResource("layouts/Home.fxml");
+        if(layoutFile == null){
+            throw new RuntimeException("Layout file for Home view not found!");
+        }
+
         try {
-            root = FXMLLoader.load(getClass().getClassLoader().getResource("layouts/Home.fxml"));
+            root = FXMLLoader.load(layoutFile);
         } catch (IOException e) {
             e.printStackTrace();
             throw new Error(e);
