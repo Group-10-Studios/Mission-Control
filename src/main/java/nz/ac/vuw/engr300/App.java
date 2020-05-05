77530fb92b736f6f0a68ebe6a52f4c5abfc6ae23
Nathan Duckett <nduckett13@gmail.com>
2020-05-05 19:13:54 +1200

    #28: Add shaded fat JAR generation - Change App to GUI superclass

--- src/main/java/nz/ac/vuw/engr300/App.java
+++ src/main/java/nz/ac/vuw/engr300/App.java
@@ -1,21 +1,9 @@
 package nz.ac.vuw.engr300;
 
-import org.apache.log4j.Logger;
+import nz.ac.vuw.engr300.gui.GUI;
 
 public class App {
-    private static final Logger logger = Logger.getLogger(App.class);
-
-    public boolean getStatus() {
-        logger.info("Getting status: True");
-        return true;
-    }
-
-    public void printHello() {
-        System.out.println("Hello World!");
-    }
-
     public static void main(String[] args) {
-        logger.info("Application Starting");
-        new App().printHello();
+        GUI.main(args);
     }
 }
