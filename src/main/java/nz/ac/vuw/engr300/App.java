a04f2c3f4626be821b3bafd341512e5f6c5ec1ff
Nathan Duckett <nduckett13@gmail.com>
2020-05-10 12:13:24 +1200

    #46: Add error handling to main application class to output a message

--- src/main/java/nz/ac/vuw/engr300/App.java
+++ src/main/java/nz/ac/vuw/engr300/App.java
@@ -1,5 +1,9 @@
 package nz.ac.vuw.engr300;
 
+import javax.swing.JOptionPane;
+
+import org.apache.log4j.Logger;
+
 import nz.ac.vuw.engr300.gui.GUI;
 
 /**
@@ -8,11 +12,32 @@ import nz.ac.vuw.engr300.gui.GUI;
  * @author Nathan Duckett
  */
 public class App {
+	private static final Logger LOGGER = Logger.getLogger(App.class);
 
     /**
      * Main application to start the GUI.
      */
     public static void main(String[] args) {
-        GUI.main(args);
+        try {
+        	GUI.main(args);
+        } catch (Error e) {
+        	showError("An unhandled error has ocurred in the application. Please view the logs to find what caused this.");
+        	// Dump entire error message into log files.
+        	LOGGER.error(e.getMessage());
+        	System.exit(1);
+        } catch (RuntimeException e) {
+        	showError("An unhandled RuntimeException has ocurred in the application. Please view the logs to find what caused this.");
+        	// Dump entire error message into log files.
+        	LOGGER.error(e.getMessage());
+        	System.exit(1);
+        }
+    }
+    
+    /**
+     * Opens a JOptionPane to display the error message. This must be done using Java swing, as
+     * it helps show the 
+     */
+    private static void showError(String errorMessage) {
+    	JOptionPane.showMessageDialog(null, errorMessage, "Mission-Control Error", JOptionPane.INFORMATION_MESSAGE);
     }
 }
