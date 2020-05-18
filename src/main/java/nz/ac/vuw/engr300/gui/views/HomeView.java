f67ccaa6b86bab5b80c6b9dadee703a960884301
Timothy Salisbury <salisbtimo@ecs.vuw.ac.nz>
2020-05-19 11:33:00 +1200

    #40: Added code to set up application close listener

--- src/main/java/nz/ac/vuw/engr300/gui/views/HomeView.java
+++ src/main/java/nz/ac/vuw/engr300/gui/views/HomeView.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import nz.ac.vuw.engr300.gui.controllers.HomeController;
 
 import java.io.IOException;
 import java.net.URL;
@@ -22,8 +23,10 @@ public class HomeView {
             throw new RuntimeException("Layout file for Home view not found!");
         }
 
+        FXMLLoader loader = new FXMLLoader(layoutFile);
         try {
-            root = FXMLLoader.load(layoutFile);
+
+            root = loader.load();
         } catch (IOException e) {
             e.printStackTrace();
             throw new Error(e);
@@ -32,6 +35,9 @@ public class HomeView {
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
+
+        HomeController controller = loader.getController();
+        stage.setOnCloseRequest(e -> controller.shutdown());
 
         stage.show();
     }
