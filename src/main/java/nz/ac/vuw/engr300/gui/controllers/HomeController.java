cfe6abc180066179ddc2fe5efb7962f3ccacc21f
Timothy Salisbury <salisbtimo@ecs.vuw.ac.nz>
2020-05-18 23:14:48 +1200

    #40: Changed range dist to acceleration | refactored

--- src/main/java/nz/ac/vuw/engr300/gui/controllers/HomeController.java
+++ src/main/java/nz/ac/vuw/engr300/gui/controllers/HomeController.java
@@ -6,10 +6,8 @@
 package nz.ac.vuw.engr300.gui.controllers;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
@@ -23,8 +21,6 @@ import javafx.util.Duration;
 import nz.ac.vuw.engr300.communications.importers.OpenRocketImporter;
 import nz.ac.vuw.engr300.communications.model.RocketStatus;
 import nz.ac.vuw.engr300.gui.components.RocketDataLineChart;
-import nz.ac.vuw.engr300.weather.importers.WeatherImporter;
-import nz.ac.vuw.engr300.weather.model.WeatherData;
 
 import javax.swing.*;
 
@@ -37,9 +33,9 @@ import javax.swing.*;
 public class HomeController implements Initializable {
     
     @FXML Label weatherLabel;
-    @FXML RocketDataLineChart lineChartAltitude;
-    @FXML RocketDataLineChart lineChartVel;
-    @FXML RocketDataLineChart lineChartRangeDist;
+    @FXML public RocketDataLineChart lineChartAltitude;
+    @FXML public RocketDataLineChart lineChartVel;
+    @FXML public RocketDataLineChart lineChartAcceleration;
 
     /**
     * This is the initialize method that is called to build the root before starting the javafx project.
@@ -48,8 +44,6 @@ public class HomeController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         WeatherController wc = new WeatherController(weatherLabel);
         wc.updateWindSpeed();
-
-
     }
 
     /**
@@ -71,39 +65,22 @@ public class HomeController implements Initializable {
     }
 
     public void runSim(){
+        OpenRocketImporter simulatorImporter = new OpenRocketImporter();
 
-//        //TODO: implement with data rather than a variable
-//        AtomicInteger var = new AtomicInteger();
-//        new Thread(() -> {
-//            while(true){
-//                try {
-//                    Thread.sleep(1000);
-//                } catch (InterruptedException e){
-//                    e.printStackTrace();
-//                }
-//                lineChartAltitude.addValue(var.getAndIncrement(), var.getAndIncrement());
-//                lineChartRangeDist.addValue(var.getAndIncrement(), var.getAndIncrement());
-//                lineChartVel.addValue(var.getAndIncrement(), var.getAndIncrement());
-//            }
-//        }).start();
-
-        OpenRocketImporter sim = new OpenRocketImporter();
-
-        JFileChooser fc = new JFileChooser("src/main/resources");
-        if(fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-            File f = fc.getSelectedFile();
-            sim.importData(f.getAbsolutePath());
-            sim.subscribeObserver((data) -> {
-
+        JFileChooser fileChooser = new JFileChooser("src/main/resources");
+        if(fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            simulatorImporter.importData(file.getAbsolutePath());
+            simulatorImporter.subscribeObserver((data) -> {
                 if (data instanceof RocketStatus) {
                     lineChartAltitude.addValue(data.getTime(), ((RocketStatus) data).getAltitude());
-                    lineChartRangeDist.addValue(data.getTime(), 1);
+                    lineChartAcceleration.addValue(data.getTime(), ((RocketStatus) data).getTotalAcceleration());
                     lineChartVel.addValue(data.getTime(), ((RocketStatus) data).getTotalVelocity());
                 }
 
             });
 
-            sim.start();
+            simulatorImporter.start();
         }
 
 
