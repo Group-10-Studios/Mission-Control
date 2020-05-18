a740b8843fec6d0b22b788ff88e03210cfd52513
JoshuaHarwood <38898198+JoshuaHarwood@users.noreply.github.com>
2020-05-18 22:39:21 +1200

    #40 graphs are now displaying data from launch

--- src/main/java/nz/ac/vuw/engr300/gui/controllers/HomeController.java
+++ src/main/java/nz/ac/vuw/engr300/gui/controllers/HomeController.java
@@ -5,6 +5,7 @@
  */
 package nz.ac.vuw.engr300.gui.controllers;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,9 +20,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.util.Duration;
+import nz.ac.vuw.engr300.communications.importers.OpenRocketImporter;
+import nz.ac.vuw.engr300.communications.model.RocketStatus;
 import nz.ac.vuw.engr300.gui.components.RocketDataLineChart;
 import nz.ac.vuw.engr300.weather.importers.WeatherImporter;
 import nz.ac.vuw.engr300.weather.model.WeatherData;
+
+import javax.swing.*;
 
 /**
  * Represents the controller for the Home application view.
@@ -33,6 +38,8 @@ public class HomeController implements Initializable {
     
     @FXML Label weatherLabel;
     @FXML RocketDataLineChart lineChartAltitude;
+    @FXML RocketDataLineChart lineChartVel;
+    @FXML RocketDataLineChart lineChartRangeDist;
 
     /**
     * This is the initialize method that is called to build the root before starting the javafx project.
@@ -65,17 +72,40 @@ public class HomeController implements Initializable {
 
     public void runSim(){
 
-        //TODO: implement with data rather than a variable
-        AtomicInteger var = new AtomicInteger();
-        new Thread(() -> {
-            while(true){
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e){
-                    e.printStackTrace();
+//        //TODO: implement with data rather than a variable
+//        AtomicInteger var = new AtomicInteger();
+//        new Thread(() -> {
+//            while(true){
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e){
+//                    e.printStackTrace();
+//                }
+//                lineChartAltitude.addValue(var.getAndIncrement(), var.getAndIncrement());
+//                lineChartRangeDist.addValue(var.getAndIncrement(), var.getAndIncrement());
+//                lineChartVel.addValue(var.getAndIncrement(), var.getAndIncrement());
+//            }
+//        }).start();
+
+        OpenRocketImporter sim = new OpenRocketImporter();
+
+        JFileChooser fc = new JFileChooser("src/main/resources");
+        if(fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            File f = fc.getSelectedFile();
+            sim.importData(f.getAbsolutePath());
+            sim.subscribeObserver((data) -> {
+
+                if (data instanceof RocketStatus) {
+                    lineChartAltitude.addValue(data.getTime(), ((RocketStatus) data).getAltitude());
+                    lineChartRangeDist.addValue(data.getTime(), 1);
+                    lineChartVel.addValue(data.getTime(), ((RocketStatus) data).getTotalVelocity());
                 }
-                lineChartAltitude.addValue(var.getAndIncrement(), var.getAndIncrement());
-            }
-        }).start();
+
+            });
+
+            sim.start();
+        }
+
+
     }
 }
