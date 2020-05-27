b98a7c09a35096c3f594f4b42b4ac43116653f67
Timothy Salisbury <salisbtimo@ecs.vuw.ac.nz>
2020-05-28 09:31:57 +1200

    #79: Made needle blue

--- src/main/java/nz/ac/vuw/engr300/gui/components/RocketDataAngle.java
+++ src/main/java/nz/ac/vuw/engr300/gui/components/RocketDataAngle.java
@@ -3,6 +3,9 @@ package nz.ac.vuw.engr300.gui.components;
 import eu.hansolo.medusa.Gauge;
 import javafx.application.Platform;
 import javafx.beans.NamedArg;
+import javafx.scene.paint.Color;
+
+import java.awt.*;
 
 /**
  * A component that can display an angle or a direction.
@@ -55,6 +58,7 @@ public class RocketDataAngle extends Gauge {
         this.setKnobColor(Gauge.DARK_COLOR);
         this.setNeedleShape(NeedleShape.FLAT);
         this.setNeedleType(NeedleType.VARIOMETER);
+        this.setNeedleColor(Color.valueOf("#4267B2"));
         this.setNeedleBehavior(NeedleBehavior.OPTIMIZED);
         this.setTickLabelColor(DARK_COLOR);
         this.setAnimated(false);
