18ad970acdd1bd1d577fae13c447a4e6068e3b48
Timothy Salisbury <salisbtimo@ecs.vuw.ac.nz>
2020-05-30 19:06:15 +1200

    #91: Made robot copy/paste instead of type | Added running sim while one is already running test

--- src/test/java/nz/ac/vuw/engr300/gui/GeneralGuiTests.java
+++ src/test/java/nz/ac/vuw/engr300/gui/GeneralGuiTests.java
@@ -23,6 +23,9 @@ import org.testfx.assertions.api.Assertions;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -77,36 +80,94 @@ public class GeneralGuiTests {
      */
     @Test
     public void test_run_simulation(FxRobot robot) {
+        runSimulation(robot, "../../test/resources/FullyCorrectTestData.csv", 500);
+
+        checkGraphValues(robot, TEST_DATA);
+    }
+
+    /**
+     * Tests that when you run a simulation while one is already running, it will
+     * stop the previous simulation and run the new one.
+     *
+     * @param robot The robot injected to run tests
+     */
+    @Test
+    public void test_running_simulation_while_simulation_running(FxRobot robot){
+        // NOTE: 250ms is not enough time for this simulation to run
+        runSimulation(robot, "../../test/resources/FullyCorrectRocketData.csv", 250);
+
+        //Also note that these two files are actually different.
+
+        // Run another simulation while one is already going
+        runSimulation(robot, "../../test/resources/FullyCorrectTestData.csv", 500);
+
+        checkGraphValues(robot, TEST_DATA);
+    }
+
+    /**
+     * Checks that the graphs displayed by the UI have been populated with the {@code expected}
+     * Rocket data.
+     *
+     * @param robot     The robot injected to run tests.
+     * @param expected  The expected data that the graphs should be populated with.
+     */
+    private static void checkGraphValues(FxRobot robot, List<RocketStatus> expected){
+        List<XYChart.Data<Number, Number>> velocityChartData = robot.lookup("#lineChartVel").queryAs(RocketDataLineChart.class).getData().get(0).getData();
+        List<XYChart.Data<Number, Number>>  altitudeChartData = robot.lookup("#lineChartAltitude").queryAs(RocketDataLineChart.class).getData().get(0).getData();
+        List<XYChart.Data<Number, Number>>  accelerationChartData = robot.lookup("#lineChartAcceleration").queryAs(RocketDataLineChart.class).getData().get(0).getData();
+
+        int expectedDataSize = expected.size();
+        assertEquals(expectedDataSize, velocityChartData.size());
+        assertEquals(expectedDataSize, altitudeChartData.size());
+        assertEquals(expectedDataSize, accelerationChartData.size());
+
+        //Now check all the values displayed by the graph are those in the simulation!
+        for(int i = 0; i < expectedDataSize; i++){
+            assertEquals(expected.get(i).getTime(), velocityChartData.get(i).getXValue());
+            assertEquals(expected.get(i).getTime(), altitudeChartData.get(i).getXValue());
+            assertEquals(expected.get(i).getTime(), accelerationChartData.get(i).getXValue());
+
+            assertEquals(expected.get(i).getTotalVelocity(), velocityChartData.get(i).getYValue());
+            assertEquals(expected.get(i).getAltitude(), altitudeChartData.get(i).getYValue());
+            assertEquals(expected.get(i).getTotalAcceleration(), accelerationChartData.get(i).getYValue());
+        }
+    }
+
+    /**
+     * Runs the simulation provided at {@code simulationFile} for {@code simulationRunTime}ms.
+     *
+     * @param robot                 The robot injected to run tests.
+     * @param simulationFile        The simulation we wish to run.
+     * @param simulationRunTime     How long we should let the simulation run for.
+     */
+    private static void runSimulation(FxRobot robot, String simulationFile, long simulationRunTime){
         robot.clickOn("#btnRunSim");
         try {
             //Let the JFileChooser open, this isn't done on the main thread so we need to sleep.
             Thread.sleep(500);
         } catch (InterruptedException ignored) { }
         //As the JFileChooser can not be tested by TestFX, we must make the robot type for us.
-        robot.type(getKeyCodes("../../test/resources/FullyCorrectTestData.csv"));
+        copyPasteString(robot, simulationFile);
+
         try {
             //Let the simulation run
-            Thread.sleep(500);
+            Thread.sleep(simulationRunTime);
         } catch (InterruptedException ignored) { }
+    }
 
-        List<XYChart.Data<Number, Number>> velocityChartData = robot.lookup("#lineChartVel").queryAs(RocketDataLineChart.class).getData().get(0).getData();
-        List<XYChart.Data<Number, Number>>  altitudeChartData = robot.lookup("#lineChartAltitude").queryAs(RocketDataLineChart.class).getData().get(0).getData();
-        List<XYChart.Data<Number, Number>>  accelerationChartData = robot.lookup("#lineChartAcceleration").queryAs(RocketDataLineChart.class).getData().get(0).getData();
+    /**
+     * Copies {@code string} to the ClipBoard and pastes it in the focused InputBox,
+     * whatever that may be.
+     *
+     * @param robot     The robot injected to run tests.
+     * @param string    The string to copy and paste.
+     */
+    private static void copyPasteString(FxRobot robot, String string){
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        StringSelection stringSelection = new StringSelection(string);
+        clipboard.setContents(stringSelection, stringSelection);
 
-        assertEquals(TEST_DATA.size(), velocityChartData.size());
-        assertEquals(TEST_DATA.size(), altitudeChartData.size());
-        assertEquals(TEST_DATA.size(), accelerationChartData.size());
-
-        //Now check all the values displayed by the graph are those in the simulation!
-        for(int i = 0; i < TEST_DATA.size(); i++){
-            assertEquals(TEST_DATA.get(i).getTime(), velocityChartData.get(i).getXValue());
-            assertEquals(TEST_DATA.get(i).getTime(), altitudeChartData.get(i).getXValue());
-            assertEquals(TEST_DATA.get(i).getTime(), accelerationChartData.get(i).getXValue());
-
-            assertEquals(TEST_DATA.get(i).getTotalVelocity(), velocityChartData.get(i).getYValue());
-            assertEquals(TEST_DATA.get(i).getAltitude(), altitudeChartData.get(i).getYValue());
-            assertEquals(TEST_DATA.get(i).getTotalAcceleration(), accelerationChartData.get(i).getYValue());
-        }
+        robot.press(KeyCode.CONTROL, KeyCode.V).release(KeyCode.CONTROL, KeyCode.V).type(KeyCode.ENTER);
     }
 
     /**
@@ -115,8 +176,10 @@ public class GeneralGuiTests {
      * @param input The string to convert.
      * @return      The JavaFX KeyCodes representing the string.
      */
-    private KeyCode[] getKeyCodes(String input){
+    private static KeyCode[] getKeyCodes(String input){
         List<KeyCode> keyCodes = new ArrayList<>();
+        // Because apparently providing a function that converts string to an array of
+        // KeyCode's is too hard.
         for(String s : input.split("")){
             if(s.equals(".")){
                 keyCodes.add(KeyCode.PERIOD);
