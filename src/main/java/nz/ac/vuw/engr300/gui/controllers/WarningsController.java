ca2791899e6a415b9a7861ba641758640a20efd7
Nalin Aswani <38874717+Nalinboi@users.noreply.github.com>
2020-06-04 03:37:28 +1200

    Added warnings
    Windspeed section in warnings will be green if there are no warnings, or red if there are. I made the threshold to be 15km/h but this can be adjusted later on.

--- src/main/java/nz/ac/vuw/engr300/gui/controllers/WarningsController.java
+++ src/main/java/nz/ac/vuw/engr300/gui/controllers/WarningsController.java
@@ -2,9 +2,9 @@ package nz.ac.vuw.engr300.gui.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import nz.ac.vuw.engr300.gui.components.RocketDataAngle;
 import nz.ac.vuw.engr300.weather.importers.WeatherImporter;
 import nz.ac.vuw.engr300.weather.model.WeatherData;
+import javafx.scene.paint.Color;
 
 import java.io.FileNotFoundException;
 
@@ -20,7 +20,7 @@ public class WarningsController {
     private Label lbWarning1;
     @FXML
     private Label lbWarning2;
-    private double maxWindSpeed = 5.0; //The maximum wind speed for UI to not throw warning.
+    private double maxWindSpeed = 15.0; //The maximum wind speed for UI to not throw warning.
 
     public WarningsController(Label w1, Label w2) {
         this.lbWarning1 = w1;
@@ -41,8 +41,13 @@ public class WarningsController {
             // windpseed * 3.6
             Double winSpeedMetric = Math.round((w.getWindSpeed() * 3.6) * 100.0) / 100.0;
             if (winSpeedMetric > maxWindSpeed) {
-                lbWarning1.setText("WINDSPEED WARNING: Expected below " + maxWindSpeed +
-                        "km/h, actual was " + winSpeedMetric + "km/h");
+                lbWarning1.setText("WINDSPEED WARNING:\n  Expected less than " + maxWindSpeed +
+                        "km/h,\n  Actual was " + winSpeedMetric + "km/h.");
+                lbWarning1.setTextFill(Color.web("#ff0000", 0.8)); //Sets it red if warnings.
+            } else {
+                lbWarning1.setText("Windspeed: Everything is ok!\n  Less than " + maxWindSpeed +
+                        "km/h,\n  Actual was " + winSpeedMetric + "km/h.");
+                lbWarning1.setTextFill(Color.web("#008000", 0.8)); //Sets it green if no warnings
             }
 
         } catch (FileNotFoundException e) {
