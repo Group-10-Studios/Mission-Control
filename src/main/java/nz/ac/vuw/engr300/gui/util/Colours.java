d58628199bbd13377cff70c02006886002b073f3
Tim Salisbury <tim.salisbury@hotmail.com>
2020-07-15 12:18:24 +1200

    #129: Added View Util code

---
+++ src/main/java/nz/ac/vuw/engr300/gui/util/Colours.java
@@ -0,0 +1,15 @@
+package nz.ac.vuw.engr300.gui.util;
+
+import javafx.scene.paint.Color;
+
+public class Colours {
+    public static final Color PRIMARY_COLOUR = Color.valueOf("#4267B2");
+    public static final Color BACKGROUND_COLOUR = Color.valueOf("#F6F6F6");
+    public static final Color TEXT_COLOUR = Color.valueOf("#FFFFFF");
+    public static final Color SHADE_COLOUR = Color.valueOf("#E0E0E0");
+    public static final Color CONTENT_BACKGROUND_COLOUR = Color.valueOf("#EEEEEE");
+
+    public static final Color WARNING_INFO = Color.valueOf("#4267B2");
+    public static final Color WARNING_WARN = Color.ORANGE;
+    public static final Color WARNING_ERROR = Color.RED;
+}
