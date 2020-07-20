9b586a3af73ce6e391fc197844ae4fbac9096b30
Nalin Aswani <38874717+Nalinboi@users.noreply.github.com>
2020-07-21 00:27:18 +1200

    #127 Added methods for readibility
    Added some methods in the constructer for information view so that it is clear for each section of the right hand side of the panel.

--- src/main/java/nz/ac/vuw/engr300/gui/views/InformationView.java
+++ src/main/java/nz/ac/vuw/engr300/gui/views/InformationView.java
@@ -28,32 +28,46 @@ public class InformationView implements View {
     public RocketBattery primaryBattery = new RocketBattery();
     public RocketBattery secondaryBattery = new RocketBattery();
 
+    /**
+     * Add Batteries and warning sections of right hand side panel.
+     * @param root The root Gridpane where we will be adding nodes to.
+     */
     public InformationView(GridPane root) {
-        this.root = root;
+        setupRoot(root);
+        setupBatteries();
+        setupWarnings();
+        setupGoNoGo();
+    }
 
-        // One column, 100 percent of width
-        UiUtil.addPercentColumns(root, 100);
-        // 20 for batteries, 60 for warnings, 20 for go/no go
-        UiUtil.addPercentRows(root, 20, 60, 20);
+    private void setupWarnings() {
+        addNodeToGrid(new Label("WARNINGS"), root, 1, 0, Pos.CENTER, Color.MAGENTA, Insets.EMPTY);
+    }
 
-        // Add Batteries and warning sections of right hand side panel
-//        addNodeToGrid(new Label("BATTERIES"), root, 0, 0, Pos.CENTER, Color.TURQUOISE, Insets.EMPTY);
-
+    private void setupBatteries() {
         // Create and populate batteries in a HBox
         HBox batteryHBox = UiUtil.createMinimumHorizontalSizeHBox(5, new Insets(10), primaryBattery, secondaryBattery);
-
         addNodeToGrid(batteryHBox, root, 0, 0, Pos.CENTER, Color.TURQUOISE, Insets.EMPTY);
-        addNodeToGrid(new Label("WARNINGS"), root, 1, 0, Pos.CENTER, Color.MAGENTA, Insets.EMPTY);
+    }
 
+    private void setupGoNoGo() {
         // Create and populate go no go at bottom of right hand side
         VBox goNoGoVBox = UiUtil.createMinimumVerticalSizeVBox(5, new Insets(10), new Button("Btn0"), new Button("Btn1"));
         // Literally just for setting background colour
         goNoGoVBox.setBackground(new Background(new BackgroundFill(Color.CADETBLUE,
-         CornerRadii.EMPTY, Insets.EMPTY)));
+                CornerRadii.EMPTY, Insets.EMPTY)));
 
         // Set it to hug the warnings above it
         GridPane.setValignment(goNoGoVBox, VPos.TOP);
         addNodeToGrid(goNoGoVBox, root, 2, 0, Insets.EMPTY);
+    }
+
+    private void setupRoot(GridPane root) {
+        this.root = root;
+
+        // One column, 100 percent of width
+        UiUtil.addPercentColumns(this.root, 100);
+        // 20 for batteries, 60 for warnings, 20 for go/no go
+        UiUtil.addPercentRows(this.root, 20, 60, 20);
     }
 
 
