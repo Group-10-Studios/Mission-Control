4dafb89f63ece983b34ffe4d032ee35643a05652
Ahad Rahman <rahmanahad@ecs.vuw.ac.nz>
2020-08-05 23:20:44 +1200

    #150: Attempting to fix button reorder

--- src/main/java/nz/ac/vuw/engr300/gui/controllers/ButtonController.java
+++ src/main/java/nz/ac/vuw/engr300/gui/controllers/ButtonController.java
@@ -2,11 +2,13 @@ package nz.ac.vuw.engr300.gui.controllers;
 
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Region;
 import nz.ac.vuw.engr300.gui.components.RocketGraph;
 import nz.ac.vuw.engr300.gui.model.GraphType;
 import nz.ac.vuw.engr300.gui.views.GraphView;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -30,69 +32,71 @@ public class ButtonController {
      */
     public void updateButtons(GraphView graphView) {
         List<String> labels = Stream.of(GraphType.values()).map(g -> g.getLabel()).collect(Collectors.toList());
-        //ButtonSelected buttonSelected = new ButtonSelected();
+        ButtonSelected buttonSelected = new ButtonSelected();
         int y = 5;
+        reorderGraphs(labels, graphView);
         for (String label : labels) {
             Button b = new Button(label);
             b.setId("btn" + label.replace(" ", ""));
             b.setLayoutY(y);
-            /*
+
             b.setOnMousePressed(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent mouseEvent) {
                     b.toFront();
                     buttonSelected.originalY = b.getLayoutY();
                     buttonSelected.nextY = b.getLayoutY() - mouseEvent.getSceneY();
+                    System.out.println(b.getText());
                 }
             });
-            b.setOnMouseDragged(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent mouseEvent) {
-                    b.setLayoutY(mouseEvent.getSceneY() + buttonSelected.nextY);
-                }
-            });
-            b.setOnMouseReleased(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent mouseEvent) {
-                    double distanceMoved = Math.abs(buttonSelected.originalY - b.getLayoutY());
-                    // If the user has dragged the button past the halfway point of a button
-                    // boundary
-                    if (distanceMoved > BUTTON_HEIGHT / 2) {
-                        String buttonBeingMovedLabel = b.getText();
-                        int indexOfButtonBeingMoved = labels.indexOf(buttonBeingMovedLabel);
-                        int indexFurther = (int) Math.floor(distanceMoved / (BUTTON_HEIGHT - 1));
-                        int indexToReplace;
-                        // Figuring out which direction the user is dragging. If this is true, the user
-                        // is dragging downwards
-                        if (buttonSelected.originalY - b.getLayoutY() < 0) {
-                            indexToReplace = indexOfButtonBeingMoved + indexFurther;
-                            if (indexToReplace > labels.size() - 1) {
-                                // If the user drags beyond the list, replace the last button
-                                indexToReplace = labels.size() - 1;
-                            }
-                        } else {
-                            indexToReplace = indexOfButtonBeingMoved - indexFurther;
-                            if (indexToReplace < 0) {
-                                indexToReplace = 0; // If the user drags above the list, replace the first button
-                            }
-                        }
-                        String btnBeingReplaced = labels.get(indexToReplace);
-                        for (Button bt : pnNavButtons) {
-                            if (bt.getText().equals(btnBeingReplaced)) {
-                                b.setLayoutY(bt.getLayoutY());
-                                bt.setLayoutY(buttonSelected.originalY);
-                            }
-                        }
-                        // Swap the two butons
-                        Collections.swap(labels, indexOfButtonBeingMoved, indexToReplace);
-                        reorderGraphs(labels, graphView);
-                    } else {
-                        // If the user barely drags the button (by mistake), then put it back
-                        b.setLayoutY(buttonSelected.originalY);
-                    }
-                }
-            });
-            */
+//            b.setOnMouseDragged(new EventHandler<MouseEvent>() {
+//                @Override
+//                public void handle(MouseEvent mouseEvent) {
+//                    b.setLayoutY(mouseEvent.getSceneY() + buttonSelected.nextY);
+//                }
+//            });
+//            b.setOnMouseReleased(new EventHandler<MouseEvent>() {
+//                @Override
+//                public void handle(MouseEvent mouseEvent) {
+//                    double distanceMoved = Math.abs(buttonSelected.originalY - b.getLayoutY());
+//                    // If the user has dragged the button past the halfway point of a button
+//                    // boundary
+//                    if (distanceMoved > BUTTON_HEIGHT / 2) {
+//                        String buttonBeingMovedLabel = b.getText();
+//                        int indexOfButtonBeingMoved = labels.indexOf(buttonBeingMovedLabel);
+//                        int indexFurther = (int) Math.floor(distanceMoved / (BUTTON_HEIGHT - 1));
+//                        int indexToReplace;
+//                        // Figuring out which direction the user is dragging. If this is true, the user
+//                        // is dragging downwards
+//                        if (buttonSelected.originalY - b.getLayoutY() < 0) {
+//                            indexToReplace = indexOfButtonBeingMoved + indexFurther;
+//                            if (indexToReplace > labels.size() - 1) {
+//                                // If the user drags beyond the list, replace the last button
+//                                indexToReplace = labels.size() - 1;
+//                            }
+//                        } else {
+//                            indexToReplace = indexOfButtonBeingMoved - indexFurther;
+//                            if (indexToReplace < 0) {
+//                                indexToReplace = 0; // If the user drags above the list, replace the first button
+//                            }
+//                        }
+//                        String btnBeingReplaced = labels.get(indexToReplace);
+//                        for (Button bt : pnNavButtons) {
+//                            if (bt.getText().equals(btnBeingReplaced)) {
+//                                b.setLayoutY(bt.getLayoutY());
+//                                bt.setLayoutY(buttonSelected.originalY);
+//                            }
+//                        }
+//                        // Swap the two butons
+//                        Collections.swap(labels, indexOfButtonBeingMoved, indexToReplace);
+//                        reorderGraphs(labels, graphView);
+//                    } else {
+//                        // If the user barely drags the button (by mistake), then put it back
+//                        b.setLayoutY(buttonSelected.originalY);
+//                    }
+//                }
+//            });
+
             b.setOnAction(e -> {
                 GraphType thisGraph = GraphType.fromLabel(label);
                 for (RocketGraph chart : graphView.getGraphs()) {
@@ -106,6 +110,7 @@ public class ButtonController {
                 }
             });
             pnNavButtons.add(b);
+            y += BUTTON_HEIGHT;
         }
 
     }
@@ -140,7 +145,7 @@ public class ButtonController {
      * @author Ahad Rahman
      */
     static class ButtonSelected {
-        //double originalY;
-        //double nextY;
+        double originalY;
+        double nextY;
     }
 }
