4167d2de9ac35e07ceef0148eaae553de89ec2d9
Ahad Rahman <rahmanahad@ecs.vuw.ac.nz>
2020-08-07 12:44:21 +1200

    #139: Test case for importing weather api key

--- src/test/java/nz/ac/vuw/engr300/importers/KeyImporterTest.java
+++ src/test/java/nz/ac/vuw/engr300/importers/KeyImporterTest.java
@@ -1,18 +1,27 @@
 package nz.ac.vuw.engr300.importers;
 
+import com.google.gson.JsonObject;
 import nz.ac.vuw.engr300.exceptions.KeyNotFoundException;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class KeyImporterTest {
 
     @Test
     public void test_getAPIKeyForWeather() throws KeyNotFoundException {
         String actual = KeyImporter.getKey("weather");
-        String expected = "cc5a7a7ec97d0a02e950717c0e632548";
+        String expected = "";
+        try {
+            JsonObject expectedJson = JsonImporter.load("src/main/resources/keys.json").getAsJsonObject();
+            expected = expectedJson.get("weather").getAsString();
+        } catch (FileNotFoundException e) {
+            fail("Keys.json not found");
+        }
         assertEquals(expected, actual);
-
     }
 
 }
