894160ebc67cb68db2ca9d8b06958015571aa44e
Ahad Rahman <rahmanahad@ecs.vuw.ac.nz>
2020-08-25 13:43:36 +1200

    #162: Setup Test class for launch parameters

--- src/test/java/nz/ac/vuw/engr300/model/LaunchParametersTest.java
+++ src/test/java/nz/ac/vuw/engr300/model/LaunchParametersTest.java
@@ -1,4 +1,11 @@
 package nz.ac.vuw.engr300.model;
 
+import org.junit.jupiter.api.Test;
+
 public class LaunchParametersTest {
+
+    @Test
+    public void test() {
+        assert 5 == 5;
+    }
 }
