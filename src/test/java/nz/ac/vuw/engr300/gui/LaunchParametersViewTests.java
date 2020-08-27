7ccc90ece6866ac09b7488db899da456e646aee7
Joshua Harwood <38898198+JoshuaHarwood@users.noreply.github.com>
2020-08-27 20:13:33 +1200

    #163 moved get keycodes from GeneralGuiTests to LaunchParametersViewTests

--- src/test/java/nz/ac/vuw/engr300/gui/LaunchParametersViewTests.java
+++ src/test/java/nz/ac/vuw/engr300/gui/LaunchParametersViewTests.java
@@ -1,6 +1,7 @@
 package nz.ac.vuw.engr300.gui;
 
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import nz.ac.vuw.engr300.gui.model.TestLaunchParameters;
 import nz.ac.vuw.engr300.gui.views.HomeView;
@@ -13,6 +14,8 @@ import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.ApplicationTest;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 @ExtendWith(ApplicationExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -43,5 +46,36 @@ public class LaunchParametersViewTests extends ApplicationTest {
     private static void clickLaunchConfig(FxRobot robot) {
         Button launchConfigBtn = robot.lookup("#launchConfig").queryAs(Button.class);
         robot.clickOn(launchConfigBtn);
+    }
+
+    /**
+     * Converts a string into JavaFX KeyCode objects, used to make the robot type.
+     *
+     * @param input The string to convert.
+     * @return The JavaFX KeyCodes representing the string.
+     */
+    private static KeyCode[] getKeyCodes(String input) {
+        List<KeyCode> keyCodes = new ArrayList<>();
+        // Because apparently providing a function that converts string to an array of
+        // KeyCode's is too hard.
+        for (String s : input.split("")) {
+            if (s.equals(".")) {
+                keyCodes.add(KeyCode.PERIOD);
+            } else if (s.equals("/")) {
+                keyCodes.add(KeyCode.SLASH);
+            } else if (Character.isUpperCase(s.charAt(0))) {
+                keyCodes.add(KeyCode.CAPS);
+                keyCodes.add(KeyCode.getKeyCode(s.toUpperCase()));
+                keyCodes.add(KeyCode.CAPS);
+            } else if (s.equals("-")) {
+                keyCodes.add(KeyCode.SUBTRACT);
+            } else if (s.equals("_")) {
+                keyCodes.add(KeyCode.UNDERSCORE);
+            } else {
+                keyCodes.add(KeyCode.getKeyCode(s.toUpperCase()));
+            }
+        }
+        keyCodes.add(KeyCode.ENTER);
+        return keyCodes.toArray(KeyCode[]::new);
     }
 }
