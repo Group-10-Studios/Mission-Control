52ec7648f8493c7c069eaf528dd16ce9b69cd6e6
Timothy Salisbury <salisbtimo@ecs.vuw.ac.nz>
2020-10-06 15:08:44 +1300

    #202: Made histograms appear | Added JavaDoc

--- src/main/java/nz/ac/vuw/engr300/gui/views/ConfigurationView.java
+++ src/main/java/nz/ac/vuw/engr300/gui/views/ConfigurationView.java
@@ -13,18 +13,25 @@ import nz.ac.vuw.engr300.gui.util.UiUtil;
 import nz.ac.vuw.engr300.model.LaunchParameters;
 import java.util.function.Consumer;
 
+/**
+ * Represents the root configuration view.
+ *
+ * @author Tim Salisbury
+ * @author Ahad Rahman
+ * @author Joshua Harwood
+ */
 public class ConfigurationView implements View {
 
     private static MonteCarloImporter monteCarloImporter = new MonteCarloImporter();
+    private static MonteCarloView monteCarloView;
     private static Tab monteCarloTab;
+
     /**
      * Display the Launch Configurations popup window.
      *
      * @param callBack Callback function to accept LaunchParameters.
      */
     public static void display(Consumer<LaunchParameters> callBack) {
-
-
         Stage popupwindow = new Stage();
         popupwindow.initModality(Modality.APPLICATION_MODAL);
         popupwindow.setTitle("Launch Parameters");
@@ -36,24 +43,34 @@ public class ConfigurationView implements View {
         GridPane monteCarloGrid = UiUtil.createGridPane(0, 0, Insets.EMPTY);
 
         Tab launchParameterTab = new Tab("Launch Parameters", launchParametersGrid);
-        monteCarloTab = new Tab("Monte Carlo", new Label("Drake"));
-        monteCarloTab.setDisable(monteCarloImporter.getTable().size() == 0);
+        monteCarloTab = new Tab("Monte Carlo", monteCarloGrid);
+
+        LaunchParameterView l = new LaunchParameterView(launchParametersGrid, callBack, monteCarloImporter, ConfigurationView::setMonteCarloTabEnabled);
+
+        monteCarloView = new MonteCarloView(monteCarloGrid, monteCarloImporter);
 
         tabPane.getTabs().addAll(launchParameterTab, monteCarloTab);
 
-        Scene scene = new Scene(tabPane, 500, 600);
+        Scene scene = new Scene(tabPane, 500, 650);
 
         popupwindow.setResizable(false);
-        LaunchParameterView l = new LaunchParameterView(launchParametersGrid, callBack, monteCarloImporter, ConfigurationView::setMonteCarloTabEnabled);
-        MonteCarloView m = new MonteCarloView(monteCarloGrid, monteCarloImporter);
 
         popupwindow.setScene(scene);
         l.initialize();
-        m.initialize();
+
+        if (monteCarloImporter.getTable().size() == 0){
+            monteCarloTab.setDisable(true);
+        } else {
+            monteCarloView.initialize();
+        }
         popupwindow.showAndWait();
     }
 
+    /**
+     * Callback function for enabling and populating the monte carlo tab.
+     */
     private static void setMonteCarloTabEnabled(){
         monteCarloTab.setDisable(false);
+        monteCarloView.initialize();
     }
 }
--- src/main/java/nz/ac/vuw/engr300/gui/views/MonteCarloView.java
+++ src/main/java/nz/ac/vuw/engr300/gui/views/MonteCarloView.java
@@ -2,14 +2,29 @@ package nz.ac.vuw.engr300.gui.views;
 
 import javafx.scene.layout.GridPane;
 import nz.ac.vuw.engr300.communications.importers.MonteCarloImporter;
+import nz.ac.vuw.engr300.gui.components.RocketDataHistogram;
+import nz.ac.vuw.engr300.gui.model.GraphType;
 import nz.ac.vuw.engr300.gui.util.UiUtil;
+import java.util.stream.Collectors;
 
+/**
+ * Represents the Monte Carlo tab.
+ *
+ * @author Tim Salisbury
+ * @author Ahad Rahman
+ * @author Joshua Harwood
+ */
 public class MonteCarloView implements View {
 
     private final GridPane root;
     private final MonteCarloImporter monteCarloImporter;
 
 
+    /**
+     *
+     * @param root                  The root tab to populate.
+     * @param monteCarloImporter    The monte carlo importer.
+     */
     public MonteCarloView(GridPane root, MonteCarloImporter monteCarloImporter) {
         this.root = root;
         this.monteCarloImporter = monteCarloImporter;
@@ -17,7 +32,22 @@ public class MonteCarloView implements View {
         UiUtil.addPercentRows(root, 50, 50);
     }
 
+    /**
+     * Initalizes the monte carlo view.
+     */
     public void initialize() {
+        root.getChildren().clear();
+        RocketDataHistogram altitudeHistogram = new RocketDataHistogram(
+                new GraphType("Monte Carlo Max Altitude", "histogram"),
+                monteCarloImporter.getTable().getColumnData("Max Altitude").stream().mapToDouble(object
+                        -> (Double)object).boxed().collect(Collectors.toList()));
 
+        RocketDataHistogram groundHitSpeedHistogram = new RocketDataHistogram(
+                new GraphType("Monte Carlo Ground Hit Velocity", "histogram"),
+                monteCarloImporter.getTable().getColumnData("Ground Hit Velocity").stream().mapToDouble(object
+                        -> (Double)object).boxed().collect(Collectors.toList()));
+
+        root.add(altitudeHistogram, 0, 0);
+        root.add(groundHitSpeedHistogram, 0, 1);
     }
 }
